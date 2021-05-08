6a3d8caa64b89358edf17be0f18c24f29768310c
Siripong <aeio.yu@gmail.com>
2021-05-09 00:50:54 +0700

    fix: refactor api client

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,12 +54,7 @@ const screens = {
 const spacing = {};
 
 module.exports = {
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}',
-    './configs/**/*.{js,ts,jsx,tsx}',
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './configs/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontSize,
