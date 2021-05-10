22df23bf6ae33d1940d32c93ae3a63ab548063f6
Siripong <aeio.yu@gmail.com>
2021-05-10 18:33:34 +0700

    fix: remove keen welcome flickity

--- components/common/GridCarousel/GridCarousel.tsx
+++ components/common/GridCarousel/GridCarousel.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useKeenSlider } from 'keen-slider/react';
-import 'keen-slider/keen-slider.min.css';
+import Flickity from 'react-flickity-component';
+import 'flickity/css/flickity.css';
 
 export interface GridCarouselProps {
   className?: string;
@@ -14,24 +14,28 @@ interface GridCarouselCompose {
   Item: React.FC;
 }
 
-const GridCarousel: React.FC<GridCarouselProps> & GridCarouselCompose = ({ className, children, ...rest }) => {
-  const [sliderRef, slider] = useKeenSlider<HTMLDivElement>({ slidesPerView: 4, spacing: 15 });
-  React.useEffect(() => {
-    if (slider) {
-      slider.refresh();
-    }
-  }, [children]);
+const flickityOptions = {
+  pageDots: false,
+  cellAlign: 'left',
+};
 
+const GridCarousel: React.FC<GridCarouselProps> & GridCarouselCompose = ({ className, children, ...rest }) => {
   return (
     <div className={className} {...rest}>
-      <div ref={sliderRef} className="keen-slider">
+      <Flickity
+        className={'carousel'} // default ''
+        elementType={'div'} // default 'div'
+        options={flickityOptions} // takes flickity options {}
+        disableImagesLoaded={false} // default false
+        reloadOnUpdate // default false
+      >
         {children}
-      </div>
+      </Flickity>
     </div>
   );
 };
 
-GridCarousel.Item = ({ children }) => <div className="keen-slider__slide">{children}</div>;
+GridCarousel.Item = ({ children }) => <div style={{ width: '25%', marginRight: 15 }}>{children}</div>;
 GridCarousel.Item.displayName = 'CarouselItem';
 
 export default GridCarousel;
--- components/common/HeroBanner/HeroBanner.tsx
+++ components/common/HeroBanner/HeroBanner.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { useKeenSlider } from 'keen-slider/react';
-import 'keen-slider/keen-slider.min.css';
+import Flickity from 'react-flickity-component';
+import 'flickity/css/flickity.css';
+
+const flickityOptions = {
+  initialIndex: 0,
+};
 
 interface Props {
   banners: {
@@ -12,48 +16,35 @@ interface Props {
 }
 
 const HeroBanner: React.FC<Props> = ({ banners = [] }) => {
-  const [currentSlide, setCurrentSlide] = React.useState(0);
-  const [sliderRef, slider] = useKeenSlider<HTMLDivElement>({
-    initial: 0,
-    slideChanged(s) {
-      setCurrentSlide(s.details().relativeSlide);
-    },
-  });
-
   return (
     <>
-      <div className="navigation-wrapper">
-        <div ref={sliderRef} className="keen-slider">
-          {banners.map((banner) => (
-            <div className="keen-slider__slide" key={banner.id}>
-              <a href={banner.url} target="blank">
-                <picture>
-                  <source srcSet={banner.desktop} media="(min-width: 768px)" />
-                  <source srcSet={banner.mobile} media="(min-width: 767px)" />
-                  <img
-                    src={banner.mobile}
-                    loading="lazy"
-                    alt={`hero banner ${banner.id}`}
-                    className="w-full"
-                    width="1264"
-                    height="440"
-                    style={{ aspectRatio: '1264 / 440' }}
-                  />
-                </picture>
-              </a>
-            </div>
-          ))}
-        </div>
-        {/* {slider && (
-          <>
-            <ArrowLeft onClick={(e) => e.stopPropagation() || slider.prev()} disabled={currentSlide === 0} />
-            <ArrowRight
-              onClick={(e) => e.stopPropagation() || slider.next()}
-              disabled={currentSlide === slider.details().size - 1}
-            />
-          </>
-        )} */}
-      </div>
+      <Flickity
+        className={'carousel'} // default ''
+        elementType={'div'} // default 'div'
+        options={flickityOptions} // takes flickity options {}
+        disableImagesLoaded={false} // default false
+        reloadOnUpdate // default false
+      >
+        {banners.map((banner) => (
+          <div style={{ width: '100%' }} key={banner.id}>
+            <a href={banner.url} target="blank">
+              <picture>
+                <source srcSet={banner.desktop} media="(min-width: 768px)" />
+                <source srcSet={banner.mobile} media="(min-width: 767px)" />
+                <img
+                  src={banner.mobile}
+                  loading="lazy"
+                  alt={`hero banner ${banner.id}`}
+                  className="w-full"
+                  width="1264"
+                  height="440"
+                  style={{ aspectRatio: '1264 / 440' }}
+                />
+              </picture>
+            </a>
+          </div>
+        ))}
+      </Flickity>
     </>
   );
 };
